9a0dd507d58c943495832841e7f8ef90250005d9
Vishal Prakash Daimane <142233215+VishalDaimane@users.noreply.github.com>
2025-01-30 10:31:02 +0530

    Update 2]Gradient_descent.py

--- 2]Gradient_descent.py
+++ 2]Gradient_descent.py
@@ -5,26 +5,25 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 # 1. Import dataset
-california = fetch_california_housing()
-data = pd.DataFrame(california.data, columns=california.feature_names)
-data['MedHouseVal'] = california.target
+data = fetch_california_housing(as_frame=True)
+df = data.frame
 
 # 2. Display first 5 rows
-print(data.head())
+print(df.head())
 
 # 3. Check for null values
-print(data.isnull().sum())
+print(df.isnull().sum())
 
 # 4. Visualize data
-data.plot.scatter(x='MedInc', y='MedHouseVal')
+df.plot.scatter(x='MedInc', y='MedHouseVal')
 
 # 5. Covariance and correlation
-print("Covariance:\n", data.cov())
-print("Correlation:\n", data.corr())
+print("Covariance:\n", df.cov())
+print("Correlation:\n", df.corr())
 
 # 6. Train-test split
-X = data.drop('MedHouseVal', axis=1)
-y = data['MedHouseVal']
+X = df.drop('MedHouseVal', axis=1)
+y = df['MedHouseVal']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # 7. Gradient Descent
